6c5927e49a6c381b171a65a78a013904707f6a5d
11030 <1103043038@qq.com>
2019-06-06 20:33:42 +0800

    主页优化

--- qizhong/index.php
+++ qizhong/index.php
@@ -58,81 +58,21 @@ $data = $pdo->query($sql)->fetchAll();
             </div>
         </div>
         <?php endforeach; ?>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="image/nvxie.jpg" alt="...">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam.Donec id elit non
-                        miporta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <h4 class="text-danger text-right">￥998</h4>
-                    <p class="text-right">
-                        <a href="#" class="btn btn-danger" role="button" style="float: left">加入收藏</a>
-                        <a href="#" class="btn btn-primary" role="button">查看详情</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="image/nvxie.jpg" alt="...">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam.Donec id elit non
-                        miporta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <h4 class="text-danger text-right">￥998</h4>
-                    <p class="text-right">
-                        <a href="#" class="btn btn-danger" role="button" style="float: left">加入收藏</a>
-                        <a href="#" class="btn btn-primary" role="button">查看详情</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="image/nvxie.jpg" alt="...">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam.Donec id elit non
-                        miporta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <h4 class="text-danger text-right">￥998</h4>
-                    <p class="text-right">
-                        <a href="#" class="btn btn-danger" role="button" style="float: left">加入收藏</a>
-                        <a href="#" class="btn btn-primary" role="button">查看详情</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="image/nvxie.jpg" alt="...">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam.Donec id elit non
-                        miporta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <h4 class="text-danger text-right">￥998</h4>
-                    <p class="text-right">
-                        <a href="#" class="btn btn-danger" role="button" style="float: left">加入收藏</a>
-                        <a href="#" class="btn btn-primary" role="button">查看详情</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="image/nvxie.jpg" alt="...">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam.Donec id elit non
-                        miporta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <h4 class="text-danger text-right">￥998</h4>
-                    <p class="text-right">
-                        <a href="#" class="btn btn-danger" role="button" style="float: left">加入收藏</a>
-                        <a href="#" class="btn btn-primary" role="button">查看详情</a>
-                    </p>
-                </div>
-            </div>
-        </div>
+<!--        <div class="col-sm-6 col-md-4">-->
+<!--            <div class="thumbnail">-->
+<!--                <img src="image/nvxie.jpg" alt="...">-->
+<!--                <div class="caption">-->
+<!--                    <h3>Thumbnail label</h3>-->
+<!--                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam.Donec id elit non-->
+<!--                        miporta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>-->
+<!--                    <h4 class="text-danger text-right">￥998</h4>-->
+<!--                    <p class="text-right">-->
+<!--                        <a href="#" class="btn btn-danger" role="button" style="float: left">加入收藏</a>-->
+<!--                        <a href="#" class="btn btn-primary" role="button">查看详情</a>-->
+<!--                    </p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
     </div>
 </div>
 <?php
